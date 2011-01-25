05f9c7bd51c54cb71b5fe5d5c0383a38e9194920
John Rummell <john@jrummell.com>
2011-01-25 00:00:20 -0500

    Starting implementing native WebForm validation based on validation attributes.

---
+++ xVal.WebForms.Demo.WebSite/BookingNative.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="BookingNative.aspx.cs" Inherits="BookingNative" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>xVal.WebForms Demo</title>
+</head>
+<body>
+    <script src="http://ajax.microsoft.com/ajax/jQuery/jquery-1.3.2.min.js" type="text/javascript"></script>
+    <script src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js"
+        type="text/javascript"></script>
+    <script src="js/ClientSidePlugins/xVal.jquery.validate.js" type="text/javascript"></script>
+    <script src="js/ClientSidePlugins/xVal.WebForms.jquery.validate.js" type="text/javascript"></script>
+    <form id="form1" runat="server">
+    <div id="divForm" runat="server">
+        <fieldset class="booking">
+            <legend>Booking</legend>
+            <asp:ValidationSummary ID="valSummary" runat="server" />
+            <label for="txtClientName">
+                Your name:</label>
+            <asp:TextBox ID="txtClientName" runat="server" />
+            <val:ModelPropertyValidator ID="valClientName" runat="server" ControlToValidate="txtClientName"
+                Display="Dynamic" PropertyName="ClientName" TypeName="xVal.WebForms.Demo.Booking" />
+            <label for="txtNumberOfGuests">
+                Number of guests:</label>
+            <asp:TextBox ID="txtNumberOfGuests" runat="server" />
+            <val:ModelPropertyValidator ID="valNumberOfGuests" runat="server" ControlToValidate="txtNumberOfGuests"
+                Display="Dynamic" PropertyName="NumberOfGuests" TypeName="xVal.WebForms.Demo.Booking" />
+            <label for="txtArrivalDate">
+                Arrival date:</label>
+            <asp:TextBox ID="txtArrivalDate" runat="server" />
+            <val:ModelPropertyValidator ID="valArrivalDate" runat="server" ControlToValidate="txtArrivalDate"
+                Display="Dynamic" PropertyName="ArrivalDate" TypeName="xVal.WebForms.Demo.Booking" />
+            <div>
+                <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" /></div>
+        </fieldset>
+    </div>
+    <div id="divSuccess" runat="server" class="success" visible="false">
+        Your trip has been booked!</div>
+    </form>
+</body>
+</html>
---
+++ xVal.WebForms/ModelPropertyValidator.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Compilation;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace xVal.WebForms
+{
+    public class ModelPropertyValidator : BaseValidator, INamingContainer
+    {
+        private Type _modelType;
+
+        /// <summary>
+        /// Gets or sets the name of the type.
+        /// </summary>
+        /// <value>
+        /// The name of the type.
+        /// </value>
+        public string TypeName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the property.
+        /// </summary>
+        /// <value>
+        /// The name of the property.
+        /// </value>
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// When overridden in a derived class, this method contains the code to determine whether the value in the input control is valid.
+        /// </summary>
+        /// <returns>
+        /// true if the value in the input control is valid; otherwise, false.
+        /// </returns>
+        protected override bool EvaluateIsValid()
+        {
+            EnsureChildControls();
+
+            foreach (BaseValidator validator in Controls)
+            {
+                validator.Validate();
+
+                if (!validator.IsValid)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Called by the ASP.NET page framework to notify server controls that use composition-based implementation to create any child controls they contain in preparation for posting back or rendering.
+        /// </summary>
+        protected override void CreateChildControls()
+        {
+            _modelType = GetModelType();
+
+            IEnumerable<ValidationAttribute> attributes = DataAnnotationsValidationRunner.GetValidators(_modelType);
+            foreach (ValidationAttribute attribute in attributes)
+            {
+                Type attributeType = attribute.GetType();
+
+                BaseValidator validator = null;
+                if (attributeType == typeof (RequiredAttribute))
+                {
+                    validator = new RequiredFieldValidator();
+                }
+                else if (attributeType == typeof (StringLengthAttribute))
+                {
+                    //TODO: set MaxLength on TextBox?
+                }
+                else if (attributeType == typeof (EnumDataTypeAttribute))
+                {
+                    //TODO: Enum type?
+                    validator = new CompareValidator
+                                    {
+                                        Operator = ValidationCompareOperator.DataTypeCheck,
+                                        Type = ValidationDataType.Integer
+                                    };
+                }
+                else if (attributeType == typeof (DataTypeAttribute))
+                {
+                    DataTypeAttribute dataTypeAttribute = (DataTypeAttribute) attribute;
+
+                    //TODO: CompareValidator or ReqularExpressionValidator
+
+                    ValidationDataType validationDataType;
+
+                    switch (dataTypeAttribute.DataType)
+                    {
+                        case DataType.Custom:
+                            break;
+                        case DataType.DateTime:
+                        case DataType.Date:
+                            validationDataType = ValidationDataType.Date;
+                            break;
+                        case DataType.Time:
+                            break;
+                        case DataType.Duration:
+                            break;
+                        case DataType.PhoneNumber:
+                            break;
+                        case DataType.Currency:
+                            validationDataType = ValidationDataType.Currency;
+                            break;
+                        case DataType.Text:
+                            validationDataType = ValidationDataType.String;
+                            break;
+                        case DataType.Html:
+                            break;
+                        case DataType.MultilineText:
+                            validationDataType = ValidationDataType.String;
+                            break;
+                        case DataType.EmailAddress:
+                            break;
+                        case DataType.Password:
+                            break;
+                        case DataType.Url:
+                            break;
+                        case DataType.ImageUrl:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+                else if (attributeType == typeof (RangeAttribute))
+                {
+                    RangeAttribute rangeAttribute = (RangeAttribute) attribute;
+
+                    // String is the default type.
+                    ValidationDataType dataType = ValidationDataType.String;
+                    if (rangeAttribute.OperandType == typeof (int))
+                    {
+                        dataType = ValidationDataType.Integer;
+                    }
+                    else if (rangeAttribute.OperandType == typeof (double))
+                    {
+                        dataType = ValidationDataType.Double;
+                    }
+                    else if (rangeAttribute.OperandType == typeof (DateTime))
+                    {
+                        dataType = ValidationDataType.Date;
+                    }
+
+                    validator = new RangeValidator
+                                    {
+                                        Type = dataType,
+                                        MinimumValue = rangeAttribute.Minimum.ToString(),
+                                        MaximumValue = rangeAttribute.Maximum.ToString()
+                                    };
+                }
+                else if (attributeType == typeof (RegularExpressionAttribute))
+                {
+                    RegularExpressionAttribute regexAttribute = (RegularExpressionAttribute) attribute;
+
+                    validator = new RegularExpressionValidator
+                                    {
+                                        ValidationExpression = regexAttribute.Pattern
+                                    };
+                }
+
+                if (validator != null)
+                {
+                    validator.ID = ID + "_val" + (Controls.Count + 1);
+                    validator.ControlToValidate = ControlToValidate;
+                    validator.Display = Display;
+                    validator.ValidationGroup = ValidationGroup;
+                    validator.ErrorMessage = attribute.ErrorMessage;
+
+                    Controls.Add(validator);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the model.
+        /// </summary>
+        /// <returns></returns>
+        private Type GetModelType()
+        {
+            if (_modelType == null)
+            {
+                if (String.IsNullOrEmpty(TypeName))
+                {
+                    throw new InvalidOperationException("TypeName must be set.");
+                }
+
+                _modelType = Type.GetType(TypeName);
+
+                if (_modelType == null)
+                {
+                    // App_Code Types are created with System.Web.Compilation.BuildManager
+                    _modelType = BuildManager.GetType(TypeName, false);
+                }
+
+                if (_modelType == null)
+                {
+                    throw new InvalidOperationException("Could not get a Type from " + TypeName);
+                }
+            }
+
+            return _modelType;
+        }
+    }
+}
