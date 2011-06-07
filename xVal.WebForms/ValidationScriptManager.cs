175028619a7dfeaea57d19ce5c17c3798ccfc4dc
John Rummell <john@jrummell.com>
2011-06-07 00:01:00 -0400

    ModelValidatorBase now implements IValidator instead of BaseValidator, added support for EnableClientScript.

--- xVal.WebForms/ValidationScriptManager.cs
+++ xVal.WebForms/ValidationScriptManager.cs
@@ -9,16 +9,16 @@ namespace xVal.WebForms
     {
         private readonly string _controlToValidateId;
         private readonly Type _registerType = typeof (ModelPropertyValidator);
-        private readonly ClientScriptManager _scriptManager;
+        private readonly Page _page;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ValidationScriptManager"/> class.
         /// </summary>
-        /// <param name="scriptManager">The script manager.</param>
+        /// <param name="page">The page.</param>
         /// <param name="controlToValidateId">The control to validate id.</param>
-        public ValidationScriptManager(ClientScriptManager scriptManager, string controlToValidateId)
+        public ValidationScriptManager(Page page, string controlToValidateId)
         {
-            _scriptManager = scriptManager;
+            _page = page;
             _controlToValidateId = controlToValidateId;
         }
 
@@ -30,19 +30,24 @@ namespace xVal.WebForms
         /// <param name="rules">The rules.</param>
         public void RegisterScripts(RuleCollection rules)
         {
-            const string validateInitScript = "$('form').validate();";
-            _scriptManager.RegisterStartupScript(_registerType, "Validate", validateInitScript, true);
+            ClientScriptManager scriptManager = _page.ClientScript;
 
+            string initScriptResourceUrl = scriptManager.GetWebResourceUrl(_registerType,ModelPropertyValidator.WebformValidateResourceName);
+            scriptManager.RegisterClientScriptInclude(_registerType, "WebformValidate", initScriptResourceUrl);
+            
+            string validateInitScript = String.Format("$(document).ready(function(){{ $webformValidate.init('#{0}'); }});{1}", _page.Form.ClientID, Environment.NewLine);
+            scriptManager.RegisterStartupScript(_registerType, "WebformValidateInit", validateInitScript, true);
+            
             StringBuilder validationOptionsScript = new StringBuilder();
-            validationOptionsScript.AppendFormat("$('#{0}').rules('add', ", _controlToValidateId);
+            validationOptionsScript.AppendFormat("$(document).ready(function(){{ $('#{0}').rules('add', ", _controlToValidateId);
 
             JavaScriptSerializer serializer = new JavaScriptSerializer();
             serializer.RegisterConverters(new[] {new RulesJavaScriptConverter()});
             serializer.Serialize(rules, validationOptionsScript);
 
-            validationOptionsScript.AppendLine(");");
+            validationOptionsScript.AppendLine("); });");
 
-            _scriptManager.RegisterStartupScript(
+            scriptManager.RegisterStartupScript(
                 _registerType, _controlToValidateId, validationOptionsScript.ToString(), true);
         }
 
