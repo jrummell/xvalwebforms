1098086860ae2bda30608424593cd133ec332d08
John Rummell <john@jrummell.com>
2011-06-13 20:50:20 -0400

    ModelPropertyValidator - added tests for Validate().

---
+++ xVal.WebForms.Tests/ContainerControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace xVal.WebForms.Tests
+{
+    /// <summary>
+    /// A <see cref="WebControl"/> that implements <see cref="INamingContainer"/>.
+    /// </summary>
+    internal class ContainerControl : WebControl, INamingContainer
+    {
+        
+    }
+}
